6b05787e11fe975b8fe77912b4ac6fe9b2ea21ae
doutib <thibault.doutre@berkeley.edu>
2016-01-14 01:51:58 -0800

    Delete randomforest.R

--- randomforest.R
+++
@@ -1,8 +0,0 @@
-require(randomForest)
-
-
-
-# Cross validation --------------------------------------------------------
-
-train = sample(1:nrow(data),nrow(data/2))
-randomForest(y= as.factor(data$Meta_Category), x=data[,-1],,subset = train, importance = TRUE)
