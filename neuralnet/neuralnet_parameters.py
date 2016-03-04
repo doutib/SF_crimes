36f86b702c39d4bef30d0237aa2543edccd2122c
doutib <thibault.doutre@gmail.com>
2016-03-03 22:13:10 -0800

    add neural network simulation

---
+++ neuralnet/neuralnet_parameters.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[7]:
+
+import numpy as np
+import itertools
+import csv
+
+
+# In[8]:
+
+## # Parameters
+prop_train    = np.array([0.50])
+method1       = np.array(["Tanh"])  #,"Rectifier","Sigmoid","ExpLin"])
+neurons1      = np.array([12,24,39,47])
+method2       = np.array(["Tanh"])  #,"Rectifier","Sigmoid","ExpLin"])
+neurons2      = np.array([0,12,24,39,47])
+decay         = np.array([0.0001])
+learning_rate = np.array([0.001])
+n_iter        = np.array([25])
+random_state  = np.array([1,2,3,4,5,6,7,8,9,10])
+
+#prop_train    = np.array([0.50])
+#method1       = np.array(["Tanh"])
+#neurons1      = np.array([1])
+#method2       = np.array(["None"])
+#neurons2      = np.array([0])
+#decay         = np.array([0.0001])
+#learning_rate = np.array([0.001])
+#n_iter        = np.array([25])
+#random_state  = np.array([1,2])
+
+parameters = np.array([prop_train,
+                       method1,
+                       neurons1,
+                       method2,
+                       neurons2,
+                       decay,
+                       learning_rate,
+                       n_iter,
+                       random_state])
+
+
+# In[9]:
+
+## # Export csv of parameters combinations
+with open('data/parameters.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    for element in itertools.product(*parameters):
+        spamwriter.writerow(element)
+
---
+++ neuralnet/neuralnet_simulation.py
@@ -0,0 +1,64 @@
+
+# coding: utf-8
+
+# In[18]:
+
+from neuralnet_function import *
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+
+# In[13]:
+
+## # Collect data
+df = pd.DataFrame.from_csv("data/data_train.csv", index_col = None)
+## # Separate labels from data
+X = df.drop(['Category'], axis = 1)
+Y = df[['Category']]
+
+## Parallelization parameters
+num_cpu = cpu_count()          # Number of clusters
+pool = Pool(processes=num_cpu) # Nb of processes
+max_t =  10*60                 # Max time to wait for each process
+
+
+# In[14]:
+
+# Import set of global parameters from parameters.csv
+parameters_filename = "data/parameters.csv"
+
+# Import parameters
+df_parameters = pd.DataFrame.from_csv(parameters_filename, index_col = None)
+indexes = np.arange(df_parameters.shape[0]+1) 
+
+def processInput(index): 
+    # Load parameters
+    parameters = np.array(df_parameters.iloc[[index]])[0]
+    # Define parameters
+    prop_train,method1,neurons1,method2,neurons2,decay,learning_rate,n_iter,random_state=parameters
+    # Run nnet
+    result = two_layers_nnet(X,
+                             Y,
+                             prop_train,
+                             method1,
+                             neurons1,
+                             method2,
+                             neurons2,
+                             decay,
+                             learning_rate,
+                             n_iter,
+                             random_state)
+    return result
+
+
+# In[ ]:
+
+## # Store result into csv
+with open('data/results.csv', 'wb') as f:
+    writer = csv.writer(f)
+    for index in indexes:
+        try:
+            writer.writerows(res.get(timeout=1))
+        except TimeoutError:
+            print "We lacked patience and got a multiprocessing.TimeoutError"
+
