de6d8c6c2b269f2fd035d1cb2c40667641b48ffe
doutib <thibault.doutre@gmail.com>
2016-03-05 19:49:37 -0800

    add print| change parameters: 2seeds| terminate pool

--- neuralnet/neuralnet_parameters.py
+++ neuralnet/neuralnet_parameters.py
@@ -1,35 +1,35 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[7]:
 
 import numpy as np
 import itertools
 import csv
 
 
-# In[2]:
+# In[8]:
 
 ## # Parameters
-prop_train    = np.array([0.50])
-method1       = np.array(["Tanh"])  #,"Rectifier","Sigmoid","ExpLin"])
-neurons1      = np.array([12,24,39,47])
-method2       = np.array(["Tanh"])  #,"Rectifier","Sigmoid","ExpLin"])
-neurons2      = np.array([0,12,24,39,47])
-decay         = np.array([0.0001])
-learning_rate = np.array([0.001])
-n_iter        = np.array([25])
-random_state  = np.array([1,2,3,4,5,6,7,8,9,10])
-
 #prop_train    = np.array([0.50])
-#method1       = np.array(["Tanh"])
-#neurons1      = np.array([1])
-#method2       = np.array(["None"])
-#neurons2      = np.array([0])
+#method1       = np.array(["Tanh"])  #,"Rectifier","Sigmoid","ExpLin"])
+#neurons1      = np.array([12,24,39,47])
+#method2       = np.array(["Tanh"])  #,"Rectifier","Sigmoid","ExpLin"])
+#neurons2      = np.array([0,12,24,39,47])
 #decay         = np.array([0.0001])
 #learning_rate = np.array([0.001])
 #n_iter        = np.array([25])
-#random_state  = np.array([1,2])
+#random_state  = np.array([1,2,3,4,5,6,7,8,9,10])
+
+prop_train    = np.array([0.50])
+method1       = np.array(["Tanh"])
+neurons1      = np.array([1])
+method2       = np.array(["None"])
+neurons2      = np.array([0])
+decay         = np.array([0.0001])
+learning_rate = np.array([0.001])
+n_iter        = np.array([25])
+random_state  = np.array([1,2])
 
 parameters = np.array([prop_train,
                        method1,
@@ -42,11 +42,20 @@ parameters = np.array([prop_train,
                        random_state])
 
 
-# In[3]:
+# In[9]:
 
 ## # Export csv of parameters combinations
 with open('data/neuralnet_parameters.csv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',')
+    spamwriter.writerow(["prop_train",
+                       "method1",
+                       "neurons1",
+                       "method2",
+                       "neurons2",
+                       "decay",
+                       "learning_rate",
+                       "n_iter",
+                       "random_state"])
     for element in itertools.product(*parameters):
         spamwriter.writerow(element)
 
--- neuralnet/neuralnet_simulation.py
+++ neuralnet/neuralnet_simulation.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[33]:
+# In[25]:
 
 from neuralnet_function import *
 from multiprocessing import Pool, TimeoutError
 from multiprocessing import cpu_count
 
 
-# In[34]:
+# In[47]:
 
 ## # Collect data
 df = pd.DataFrame.from_csv("data/data_train.csv", index_col = None)
@@ -17,21 +17,18 @@ X = df.drop(['Category'], axis = 1)
 Y = df[['Category']]
 
 
-# In[35]:
+# In[44]:
 
 # Import set of global parameters from parameters.csv
 parameters_filename = "data/neuralnet_parameters.csv"
 
 # Import parameters
 df_parameters = pd.DataFrame.from_csv(parameters_filename, index_col = None)
-indexes = np.arange(df_parameters.shape[0]+1) 
-
-# Open file
-f = open('data/neuralnet_results.csv', 'wb')
-writer = csv.writer(f)
+indexes = np.arange(df_parameters.shape[0]) 
+print df_parameters
 
 
-# In[11]:
+# In[35]:
 
 def processInput(index): 
     # Load parameters
@@ -50,20 +47,48 @@ def processInput(index):
                              learning_rate,
                              n_iter,
                              random_state)
-    writer.writerows(result)
+    return result
 
 
-# In[19]:
+# In[48]:
 
 ## Parallelization parameters
 num_cpu = cpu_count()          # Number of clusters
-max_t =  10*60                 # Max time to wait for each process
+print "Number of identified clusters: %s." %num_cpu
 
 
-# In[44]:
+# In[49]:
 
-print 'Starting clusters...'
-pool = Pool(processes=num_cpu)                 # Start clusters
-print 'Starting process...'
-pool.map(processInput, indexes)      
+print 'Start clusters...'
+pool = Pool(processes=num_cpu)                 # Start clusters  
+results = pool.map(processInput, indexes) 
+pool.terminate()
+
+
+# In[59]:
+
+print 'Results:\n--------'
+print results
+
+
+# In[61]:
+
+# Open file
+with open('data/neuralnet_results.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerow( ["logloss",
+                       "miss_err",
+                       "prec",
+                       "recall",
+                       "f1",
+                       "method1",
+                       "neurons1",
+                       "method2",
+                       "neurons2",
+                       "decay",
+                       "learning_rate",
+                       "n_iter",
+                       "random_state",
+                       "prop_train"] )
+    writer.writerows(results)
 
