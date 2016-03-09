87af94cc22c05dfd23cc1bb0c27e6343b6a625d2
doutib <thibault.doutre@gmail.com>
2016-03-09 09:24:24 -0800

    Include parameters into simulation

--- neuralnet/neuralnet_simulation.py
+++ neuralnet/neuralnet_simulation.py
@@ -1,40 +1,68 @@
 
 # coding: utf-8
 
-# In[25]:
+# In[8]:
 
 from neuralnet_function import *
 from multiprocessing import Pool, TimeoutError
 from multiprocessing import cpu_count
+import numpy as np
+import itertools
+import csv
 
 
-# In[47]:
+# In[9]:
 
 ## # Collect data
 df = pd.DataFrame.from_csv("data/data_train.csv", index_col = None)
-## # Separate labels from data
+
+# Separate labels from data
 X = df.drop(['Category'], axis = 1)
 Y = df[['Category']]
 
 
-# In[44]:
+# In[10]:
 
-# Import set of global parameters from parameters.csv
-parameters_filename = "data/neuralnet_parameters.csv"
+## # Define set of global parameters
 
-# Import parameters
-df_parameters = pd.DataFrame.from_csv(parameters_filename, index_col = None)
-indexes = np.arange(df_parameters.shape[0]) 
-print df_parameters
+# Parameters
+prop_train    = np.array([0.50])
+method1       = np.array(["Tanh"])#,"Rectifier","Sigmoid","ExpLin"])
+neurons1      = np.array([12,24,39,47])
+method2       = np.array(["Tanh"])#,"Rectifier","Sigmoid","ExpLin"])
+neurons2      = np.array([12,24,39,47])
+decay         = np.array([0.0001])
+learning_rate = np.array([0.001])
+n_iter        = np.array([25])
+random_state  = np.array([1])
+
+# Combinations
+param = np.array([prop_train,
+                       method1,
+                       neurons1,
+                       method2,
+                       neurons2,
+                       decay,
+                       learning_rate,
+                       n_iter,
+                       random_state])
+
+parameters = np.array(list(itertools.product(*param)))
+
+indexes = len(parameters)
+print "Number of sets of parameters: %s.\n" %indexes
+
+print 'Parameters:\n-----------'
+parameters
+
 
 
 # In[35]:
 
 def processInput(index): 
-    # Load parameters
-    parameters = np.array(df_parameters.iloc[[index]])[0]
-    # Define parameters
-    prop_train,method1,neurons1,method2,neurons2,decay,learning_rate,n_iter,random_state=parameters
+    # Define parameters names
+    prop_train,method1,neurons1,method2,neurons2,decay,learning_rate,n_iter,random_state=parameters[index]
+    
     # Run nnet
     result = two_layers_nnet(X,
                              Y,
@@ -50,31 +78,30 @@ def processInput(index):
     return result
 
 
-# In[64]:
-
-## Parallelization parameters
-num_cpu = cpu_count()          # Number of clusters
-print "Number of identified CPUs: %s." %num_cpu
-num_clusters = min(num_cpu,df_parameters.shape[0])
-
-
 # In[49]:
 
-print 'Start %s clusters.' % num_clusters
-pool = Pool(processes=num_clusters)                 # Start clusters  
+## # Parallelization
+
+# Number of clusters
+num_cpu = cpu_count()          
+print "Number of identified CPUs: %s.\n" %num_cpu
+num_clusters = min(num_cpu,df_parameters.shape[0])
+
+# Start clusters
+print 'Start %s clusters.\n' % num_clusters
+pool = Pool(processes=num_clusters)                   
 results = pool.map(processInput, indexes) 
 pool.terminate()
 
-
-# In[59]:
-
+# Results
 print 'Results:\n--------'
 print results
 
 
 # In[62]:
 
-# Open file
+print 'Write into csv...'
+## # Write into csv file
 with open('data/neuralnet_results.csv', 'wb') as f:
     writer = csv.writer(f)
     writer.writerow( ["logloss",
@@ -96,5 +123,5 @@ with open('data/neuralnet_results.csv', 'wb') as f:
 
 # In[ ]:
 
-
+print 'Done.'
 
